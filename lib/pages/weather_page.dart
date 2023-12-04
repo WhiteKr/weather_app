dc01db8815c6ec293cd6f96b93f62a17e85ba75d
WhiteKr <hanjisu2215g@gmail.com>
2023-12-04 21:11:32 +0900

    update: weather page

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
